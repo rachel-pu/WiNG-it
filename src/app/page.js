7ba568bf93f7f61aeb822b51658481c0089fb4d3
rachel pu <rrachelypu@gmail.com>
2024-10-07 14:38:20 -0400

    added a lot of new fonts and also configured home page

--- src/app/page.js
+++ src/app/page.js
@@ -3,31 +3,35 @@ import Image from "next/image";
 export default function Home() {
   return (
       // background grid
-      <div
-          className="relative h-screen w-full bg-colorF3F1EA bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px] flex justify-center items-center">
-          {/* Main content box */}
-          <div className="bg-gradient-to-r from-color8BCA67/60 via-colorBDE3E3/60 to-color5C9CF5/60 w-11/12 h-11/12 p-20 rounded-lg shadow-lg relative">
-              {/* Sticky note section */}
+      <div className="relative h-screen w-full bg-colorF3F1EA bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px] flex justify-center items-center">
+          {/* main content box */}
+          <div
+              className="mx-auto bg-gradient-to-r from-color8BCA67/40 via-colorBDE3E3/40 to-color5C9CF5/40 w-12.75/15 h-11/12 p-20 rounded-lg shadow-lg relative">
+
+              {/* sticky note section */}
               <div className="absolute top-2 right-2 bg-yellow-300 p-2 rounded shadow-lg">
-                  <p className="text-sm font-bold text-black">why WING.it?<br/>who are we?</p>
+                  <p className="text-sm font-bold text-color282523">why WING.it?<br/>who are we?</p>
               </div>
 
-              <div className="">
-                  <h2 className="text-4xl">
+              {/* ----------------- title text ----------------- */}
+              <div className="text-color282523 pl-1/6.5 mb-1/20">
+                  <h2 className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl -mb-1/15 mt-1/25 font-dm-sans-semibold tracking-tighter">
                       Are you ready to
                   </h2>
-                  {/* Title  */}
-                  <h1 className="text-9xl font-bold mb-4">
+                  {/* title */}
+                  <h1 className="text-4xl sm:text-5xl md:text-10xl lg:text-10xl xl:text-12xl -mb-1/15 font-dm-sans-black tracking-tighter">
                       WiNG.it
                   </h1>
-                  <p className="text-xl mb-6">
+                  <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl mb-1/15 font-dm-sans-semibold tracking-tighter">
                       so you don&apos;t need to wing it?
                   </p>
-                  <p className="text-md mb-8">
-                      It&apos;s time to be Well-prepared for Interviews, Networking, and being Great!
-                  </p>
               </div>
-              {/* Button */}
+
+              {/* subtext */}
+              <p className="text-2xl tracking-tight mb-8 text-center text-color282523 font-satoshi">
+                  It&apos;s time to be Well-prepared for Interviews, Networking, and being Great!
+              </p>
+              {/* ----------------- button ----------------- */}
               <button className="bg-blue-500 hover:bg-blue-600 text-white py-3 px-8 rounded-lg font-semibold">
                   I&apos;m Ready!
               </button>
