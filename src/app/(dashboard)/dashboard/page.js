27ddf526cdb4eb8b936e0b0da03363bb03efe44e
rachel pu <117247077+rachel-pu@users.noreply.github.com>
2025-08-26 16:08:23 -0400

    [WINGIT-13] adjusting css for dashboard (#8)
    * added new file and adjusted inline css
    
    * deleted unused imports
    
    * adjusted name of page_template
    
    * adjusted name again
    
    * added use client in leftnavbar file

--- src/app/(dashboard)/dashboard/page.js
+++ src/app/(dashboard)/dashboard/page.js
@@ -1,31 +1,26 @@
 "use client";
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
 import CssBaseline from '@mui/material/CssBaseline';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import LeftNavbar from '/components/LeftNavbar';
-import Stack from "@mui/material/Stack";
 import Grid from '@mui/material/Grid2';
 import DashboardCard from '../dashboard/(components)/DashboardCard';
-import {Button, CardActions} from "@mui/material";
-import MainAppBar from "../../../../components/MainAppBar";
-import Page_Template from "../../page_template";
-import { ClerkProvider, SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+
+import DefaultAppLayout from "../../DefaultAppLayout";
+import {  SignedIn } from '@clerk/nextjs'
+
+import "./Dashboard.css";
 
 export default function Dashboard() {
     return (
         <SignedIn>
             <Box sx={{ display: 'flex' }}>
                 <CssBaseline />
-                <Page_Template title="Dashboard" color="#2850d9">
+                <DefaultAppLayout title="Dashboard" color="#2850d9">
                     {/* --------- main content --------- */}
                     <Box
                         component="main"
-                        sx={{ flexGrow: 1, bgcolor: '#F3F1EB', p: 3, height: '100vh', overflow: 'auto' }}
+                        className="main-dashboard-container"
                     >
-                        <Toolbar />
                     {/*  --------- grid ---------  */}
                         <Box sx={{ flexGrow: 1 }}>
                             <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 1, sm: 1, md: 2, lg: 3}}>
@@ -86,7 +81,7 @@ export default function Dashboard() {
                             </Grid>
                         </Box>
                     </Box>
-                </Page_Template>
+                </DefaultAppLayout>
             </Box>
         </SignedIn>
     );
