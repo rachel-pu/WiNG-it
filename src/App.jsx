07ee74fda01d84eebb476038e563eb61cea21825
rachel-pu <rpu@ufl.edu>
2025-10-09 19:41:08 -0400

    fixed imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route, Link } from 'react-router-dom';
-import Home from './App/Pages/Home/Home.jsx';
-import Settings from './App/Pages/Settings/Settings.jsx';
-import Signin from './App/Pages/Auth/Signin/Signin.jsx';
-import Signup from './App/Pages/Auth/Signup/Signup.jsx';
-import Dashboard from './App/Pages/Dashboard/Dashboard.jsx';
-import Behavioral from './App/Pages/Tools/Behavioral/Behavioral.jsx';
-import InterviewResults from "./App/Pages/Tools/Behavioral/results/result.jsx"
+import Home from './app/pages/Home/Home.jsx';
+import Settings from './app/pages/Settings/Settings.jsx';
+import Signin from './app/pages/Auth/Signin/Signin.jsx';
+import Signup from './app/pages/Auth/Signup/Signup.jsx';
+import Dashboard from './app/pages/Dashboard/Dashboard.jsx';
+import Behavioral from './app/pages/Tools/Behavioral/Behavioral.jsx';
+import InterviewResults from "./app/pages/Tools/Behavioral/results/result.jsx"
 import ProtectedRoute from './components/ProtectedRoute.jsx'; 
 
 function App() {
