3745eb64d1dcee3f79fa0160f65d1064e335c71d
clynae2 <48706019+Lyra126@users.noreply.github.com>
2025-10-11 18:31:31 -0400

    removed supabase console.log

--- supabase.js
+++ supabase.js
@@ -3,6 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-console.log('Supabase URL at runtime:', supabaseUrl)
-
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
