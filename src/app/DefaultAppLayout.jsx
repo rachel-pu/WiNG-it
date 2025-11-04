eb795434946c0c7f8814d59b15f06307e73a5bf0
rachel-pu <rpu@ufl.edu>
2025-11-03 18:42:37 -0600

    removing statistics from app navbar for now

--- src/app/DefaultAppLayout.jsx
+++ src/app/DefaultAppLayout.jsx
@@ -61,11 +61,11 @@ const DefaultAppLayout = ({ children }) => {
       href: "/behavioral",
       icon: <HiChatBubbleLeftRight className="h-6 w-6 shrink-0 text-neutral-700 dark:text-neutral-200" />,
     },
-    {
-      label: "Statistics",
-      href: "/statistics",
-      icon: <HiChartBar className="h-6 w-6 shrink-0 text-neutral-700 dark:text-neutral-200" />,
-    },
+    // {
+    //   label: "Statistics",
+    //   href: "/statistics",
+    //   icon: <HiChartBar className="h-6 w-6 shrink-0 text-neutral-700 dark:text-neutral-200" />,
+    // },
     {
       label: "Settings",
       href: "/settings/profile",
