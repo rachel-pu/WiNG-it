80319c9cd8e2e1aa6c44c415a9b2e463b7566e5b
rachel-pu <rpu@ufl.edu>
2025-11-12 20:48:40 -0500

    adjusted href of settings in defaultappylayout

--- src/app/DefaultAppLayout.jsx
+++ src/app/DefaultAppLayout.jsx
@@ -68,7 +68,7 @@ const DefaultAppLayout = ({ children }) => {
     },
     {
       label: "Settings",
-      href: "/settings/profile",
+      href: "/settings",
       icon: <IoMdSettings className="h-6 w-6 shrink-0 text-neutral-700 dark:text-neutral-200" />,
     },
   ];
